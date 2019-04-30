94072073a1e173775e3f5fec26c451f21256d883
Oleh Sabaniuk <olega007@gmail.com>
2019-04-30 11:28:14 +0300

    changed location of media files in settings/base.py from .. to ../..

--- studentsdb/settings/base.py
+++ studentsdb/settings/base.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../..', 'media')
 
 # email settings
 ADMIN_EMAIL = '...'
